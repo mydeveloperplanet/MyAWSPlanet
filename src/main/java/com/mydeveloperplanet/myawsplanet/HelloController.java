03cc30d70102b14621d6ca25318fbae9d3e2926d
mydeveloperplanet <gunter@mydeveloperplanet.com>
2021-05-29 10:38:28 +0200

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myawsplanet/HelloController.java
@@ -0,0 +1,24 @@
+package com.mydeveloperplanet.myawsplanet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        String message = "Hello AWS!";
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message += " From host: " + ip;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+}
---
+++ src/main/java/com/mydeveloperplanet/myawsplanet/MyAWSPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myawsplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAWSPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAWSPlanetApplication.class, args);
+	}
+
+}
